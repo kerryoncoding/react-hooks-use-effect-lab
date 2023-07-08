948e9c5904fb1f59f2eaefc71f95eda7f4a2567b
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-07 20:37:22 -0400

    countdown-timer is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Question from "./Question";
 import quiz from "../data/quiz";
 
 function App() {
+  const [count, setCount] = useState(0)
   const [questions, setQuestions] = useState(quiz);
   const [currentQuestionId, setCurrentQuestion] = useState(1);
   const [score, setScore] = useState(0);
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+
+  
 
   function handleQuestionAnswered(correct) {
     if (currentQuestionId < questions.length) {
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=> {
+    const timer = setTimeout(() => {
+      if (timeRemaining === 1) {
+        console.log("here")
+        onAnswered(false)
+        setTimeRemaining(10)
+
+      } else {
+       if (timeRemaining > 0) {
+        setTimeRemaining(()=> timeRemaining-1)
+      }}
+    console.log(timeRemaining)  
+   }, 1000);
+   
+   return () => clearTimeout(timer);  
+   
+  }, [timeRemaining])
+  
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
